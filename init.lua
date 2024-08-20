fd7671672448071d1be0daac2beddbedf46257b0
Abdullah-Khawahir <abdullah.khawahir@gmail.com>
2024-08-20 20:27:33 +0300

    'update'

--- init.lua
+++ init.lua
@@ -214,20 +214,6 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>w', group = '[W]orkspace' },
-        -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        -- ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        -- ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        -- { '<leader>c_', hidden = true },
-        -- { '<leader>d_', hidden = true },
-        -- { '<leader>h_', hidden = true },
-        -- { '<leader>r_', hidden = true },
-        -- { '<leader>s_', hidden = true },
-        -- { '<leader>t_', hidden = true },
-        -- { '<leader>w_', hidden = true },
       }
       -- visual mode
       require('which-key').add({
